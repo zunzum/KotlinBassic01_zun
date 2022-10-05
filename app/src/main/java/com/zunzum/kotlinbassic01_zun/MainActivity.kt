c049e7ba4518e77dc9b7c6197ef2cd5376858ab8
zunzum <zunzum@naver.com>
2022-10-05 22:14:40 +0900

    로그 찍어보기

--- app/src/main/java/com/zunzum/kotlinbassic01_zun/MainActivity.kt
+++ app/src/main/java/com/zunzum/kotlinbassic01_zun/MainActivity.kt
@@ -2,6 +2,7 @@ package com.zunzum.kotlinbassic01_zun
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -9,7 +10,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        clickBtn.setOnClickListener {
 
-    }
+            // 코드에 메모 남기 - 주석
+//            ctrl + / : 커서가 있는 줄을 주석처리
+//            이 { } 안에 있는 코드만, 버튼이 눌렸을때 실행됨
+
+            Log.d("메인화면", "클릭용 버튼 눌림")
+        }
+   }
 
 }
